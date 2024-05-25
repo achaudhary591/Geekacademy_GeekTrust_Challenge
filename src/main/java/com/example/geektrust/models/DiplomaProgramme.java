baccd1bd2906216fe7bd2be460ff418b8950b79c
achaudhary591 <achaudhary591@gmail.com>
2024-05-25 10:52:59 +0530

    Removed duplication

--- src/main/java/com/example/geektrust/models/DiplomaProgramme.java
+++ src/main/java/com/example/geektrust/models/DiplomaProgramme.java
@@ -3,57 +3,9 @@ package com.example.geektrust.models;
 import com.example.geektrust.enums.ProgrammeCategoryEnum;
 import com.example.geektrust.helpers.Constants;
 
-public class DiplomaProgramme extends BaseModel implements Programme {
-
-    private final ProgrammeCategoryEnum programmeCategoryEnum;
-    private final Double programmeDiscount;
-    private Double programmeFees;
-    private Integer count;
-    private Double programmeDiscountAmount;
+public class DiplomaProgramme extends AbstractProgramme {
 
     public DiplomaProgramme() {
-        programmeCategoryEnum = ProgrammeCategoryEnum.DIPLOMA;
-        programmeFees = Constants.DIPLOMA_FEES;
-        count = Constants.INTEGER_INITIALIZE_VALUE;
-        programmeDiscount = Constants.DIPLOMA_DISCOUNT;
-        programmeDiscountAmount = Constants.DOUBLE_INITIALIZE_VALUE;
+        super(ProgrammeCategoryEnum.DIPLOMA, Constants.DIPLOMA_FEES, Constants.DIPLOMA_DISCOUNT);
     }
-
-    @Override
-    public ProgrammeCategoryEnum getProgramme() {
-        return programmeCategoryEnum;
-    }
-
-    @Override
-    public Double getProgrammeFee() {
-        return programmeFees;
-    }
-
-    @Override
-    public void addProgramme(Integer quantity) {
-        count += quantity;
-
-    }
-
-    @Override
-    public Integer getProgrammeCount() {
-        return count;
-    }
-
-    @Override
-    public Double getProgrammeDiscount() {
-        return programmeDiscount;
-    }
-
-    @Override
-    public void addProMembershipDiscountCoupon() {
-        programmeDiscountAmount = programmeFees * programmeDiscount;
-        programmeFees = programmeFees * (1 - programmeDiscount);
-    }
-
-    @Override
-    public Double getProgrammeDiscountAmount() {
-        return programmeDiscountAmount;
-    }
-
 }
